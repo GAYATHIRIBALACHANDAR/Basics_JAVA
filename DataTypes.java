04bfbe6b03df37634f5bbf1f2db82a65e80dd4ba
GAYATHIRI <gayathiribala15@gmail.com>
2025-06-15 18:22:53 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,25 @@
+public class DataTypes {
+    public static void main(String []args){
+        /*the data types of the java are in three ways:
+         * 1.PRIMITIVE = Stored directly in stack (fast, simple).
+         *     byte
+         *     short
+         *     int 
+         *     float
+         *     long
+         *     double
+         *     char
+         *     boolean
+         * 
+         * 2.NON-PRIMITIVE =Stores references(address) in stack,real object in heap #when we create the variable with the new keyword that is when we int []a={1,2,3,4}
+         *                 it stores the variable (address or reference) stack but the values are stored in the heap
+         *     arrays
+         *     string
+         *     classes
+         *     interfaces
+         *     enums
+         *     
+         */
+    }
+    
+}
